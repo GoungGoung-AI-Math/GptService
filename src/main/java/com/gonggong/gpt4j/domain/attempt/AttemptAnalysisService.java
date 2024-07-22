7dd9e9cad82f1fa373ed87c5320c86883996ca6a
ohchansol <haxr369@gmail.com>
2024-07-23 01:17:34 +0900

    Feat : 텍스트 기반 풀이에 대한 분석 응답 구현 To Do 이미지 기반에 대한 분석 구현

--- src/main/java/com/gonggong/gpt4j/domain/attempt/AttemptAnalysisService.java
+++ src/main/java/com/gonggong/gpt4j/domain/attempt/AttemptAnalysisService.java
@@ -1,8 +1,11 @@
 package com.gonggong.gpt4j.domain.attempt;
 
+import com.example.demo.my.kafka.infra.kafka.dtos.AnalysisType;
 import com.example.demo.my.kafka.infra.kafka.dtos.MessageType;
 import com.example.demo.my.kafka.infra.kafka.dtos.attempt.analysis.AttemptAnalysisRequestDto;
+import com.example.demo.my.kafka.infra.kafka.dtos.attempt.analysis.AttemptAnalysisResponseDto;
 import com.gonggong.gpt4j.domain.VisionReqDto;
+import com.gonggong.gpt4j.domain.attempt.event.AttemptAnalysisResponseEvent;
 import com.gonggong.gpt4j.domain.attempt.publisher.kafka.AttemptAnalysisResponsePublisher;
 import com.gonggong.gpt4j.domain.chat.ChatAIService;
 import com.gonggong.gpt4j.domain.chat.req.EncodedImage;
@@ -14,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,11 +29,12 @@ public class AttemptAnalysisService {
     private final AttemptAnalysisResponsePublisher attemptAnalysisResponsePublisher;
 
     public void attemptAnalysisForMath(AttemptAnalysisRequestDto dto){
-        Content analysis = chatAIService.visionMathQuery(map(dto));
-
+        Content analysis = chatAIService.visionMathQuery(mapRequestToDto(dto));
+        AttemptAnalysisResponseEvent event = mapContentToResponseEvent(dto.getAttemptId(), analysis);
+        attemptAnalysisResponsePublisher.publish(event);
     }
 
-    public VisionReqDto map(AttemptAnalysisRequestDto dto) {
+    public VisionReqDto mapRequestToDto(AttemptAnalysisRequestDto dto) {
         List<ReqContent> reqContents = dto.getContent().stream()
                 .map(content -> {
                     // `messageType`에 따라 적절한 `ReqContent` 타입을 선택합니다.
@@ -44,5 +49,21 @@ public class AttemptAnalysisService {
                 .collect(Collectors.toList());
 
         return new VisionReqDto(reqContents);
+    }
+
+    public AttemptAnalysisResponseEvent mapContentToResponseEvent(Long attemptId, Content content){
+        AttemptAnalysisResponseDto dto = AttemptAnalysisResponseDto.builder()
+                .attemptId(attemptId)
+                .analysisType(AnalysisType.ATTEMPT)
+                .messageType(MessageType.TEXT)
+                .content(content.getMessage().getValue())
+                .build();
+        return AttemptAnalysisResponseEvent.builder()
+                .attemptAnalysisDto(dto)
+                .attemptAnalysisEventDomainEventPublisher(attemptAnalysisResponsePublisher)
+                .createdAt(ZonedDateTime.now())
+                .failureMessages(List.of())
+                .build();
+
     }
 }
